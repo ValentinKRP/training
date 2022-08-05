b17fcac5f0fdb5cf2a64fac6f83bb1cd8965ef52
ValentinCarp <32415675+ValentinKRP@users.noreply.github.com>
2022-08-05 16:29:43 +0300

    {#9}{orders.php}

---
+++ order.php
@@ -0,0 +1,69 @@
+<?php
+
+include 'common.php';
+
+$lang = translate();
+include "languages/$lang.php";
+$conn = connectDB();
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * from `orders` WHERE order_id=? ";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+    $order = $stmt->fetch(PDO::FETCH_ASSOC);
+    $sql = "SELECT * from `users_orders` WHERE order_id=? ";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+    $order_products = array();
+    $order_items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($order_items as $key => $values) {
+        $sql = "SELECT * from `products` WHERE product_id=? ";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$values['product_id']]);
+        $order_item = $stmt->fetch(PDO::FETCH_ASSOC);
+        array_push($order_products, $order_item);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Order</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <h3><?= $lang['order_id'] ?> :<?= $order['order_id'] ?></h1>
+            <h3><?= $lang['order_client'] ?> : <?= $order['user_name'] ?></h1>
+                <ul class="proditems">
+                    <?php foreach ($order_products as $r) : ?>
+                        <li>
+                            <div class="proditem">
+                                <div class="prodimage">
+                                    <img src="uploads/<?= $r['product_image'] ?>">
+                                </div>
+                                <div class="proddetails">
+                                    <ul>
+                                        <li><?= $lang['product_title'] ?>: <?= $r['title'] ?></li>
+                                        <li><?= $lang['product_description'] ?>: <?= $r['description'] ?></li>
+                                        <li><?= $lang['product_price'] ?>: <?= $r['price'] ?>$ </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <a href="orders.php"><?= $lang['navigate_orders'] ?></a>
+    </div>
+</body>
+
+</html>
---
+++ orders.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'common.php';
+
+session_start();
+
+$lang=translate();
+include "languages/" . $lang . ".php";
+
+$conn = connectDB();
+$stmt = $conn->prepare('SELECT * from orders');
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Orders</title>
+</head>
+<body>
+    <h1><?= $lang['order'] ?></h1>
+<div class="container">
+        <ul class="proditems">
+            <?php foreach ($result as $r) : ?>
+                <li>
+
+                    <div class="proditem">
+                        <div class="prodimage">
+                            <?= $r['user_name'] ?>
+                        </div>
+                        <div class="proddetails">
+                            <ul>
+                                <li><?= $lang['order_details'] ?>: <?= $r['details'] ?></li>
+                                <li><?= $lang['order_price'] ?>: <?= $r['price'] ?></li>
+                                <li><?= $lang['order_date'] ?>: <?= $r['order_date'] ?> </li>
+                            </ul>
+                        </div>
+                        <form action="order.php?id=<?= $r['order_id']?>" method="POST">
+                            <div class="viewbutton">
+                                <button type="submit" name="view"><?= $lang['view'] ?></button>
+                                <input type="hidden" name="order_id" value="<?= $r['order_id'] ?>">
+
+                            </div>
+                        </form>
+                       
+                    </div>
+
+                </li>
+                <?php endforeach; ?>
+
+
+           
+        </ul>
+       
+    </div>    
+
+</body>
+</html>
