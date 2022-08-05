27557d09544f3988d9d2eaa00bc4363987c81e3c
ValentinCarp <32415675+ValentinKRP@users.noreply.github.com>
2022-08-05 16:28:04 +0300

    {#4}{index.php}

--- index.php
+++ index.php
@@ -1,60 +1,61 @@
 <?php
+
 include 'common.php';
 
 session_start();
 
-if (!isset($_SESSION['lang'])) {
-    $_SESSION['lang'] = 'en';
-} else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang']  && !empty($_GET['lang'])) {
-    if ($_GET['lang'] == 'en') {
-        $_SESSION['lang'] = 'en';
-    } else if ($_GET['lang'] == 'ro') {
-        $_SESSION['lang'] = 'ro';
-    }
-}
 
-require_once "languages/" . $_SESSION['lang'] . ".php";
+$lang = translate();
+include "languages/$lang.php";
 
 $conn = connectDB();
 
-$user_data=check_login($conn);
 
-$stmt = $conn->prepare('SELECT * from products');
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-if (isset($_SESSION['cart'])) {
+
+if (isset($_SESSION['cart']) && count($_SESSION['cart']) !== 0) {
     $product_ids = array_column($_SESSION['cart'], 'product_id');
-}else{
-    $product_ids=array();
+    $result = array();
+
+    $exclude_ids = array_values($product_ids);
+
+    $in = implode(',', array_map('intval', $exclude_ids));
+
+    $sql = "SELECT * from products WHERE product_id NOT IN ($in)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    $product_ids = array();
+    $stmt = $conn->prepare('SELECT * from products');
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 if (isset($_POST['add'])) {
     if (isset($_SESSION['cart'])) {
-        
-
+        if (!(in_array($_POST['product_id'], $product_ids))) {
             $count = count($_SESSION['cart']);
             $product = array(
-                'product_id' => $_POST['product_id'],
-                'quantity'=> 1
+                'product_id' => $_POST['product_id']
+
             );
-          array_push($_SESSION['cart'],$product);
-            array_push($product_ids,$_POST['product_id']);
-            
-        
+            array_push($_SESSION['cart'], $product);
+            array_push($product_ids, $_POST['product_id']);
+        } else {
+            header("Location: index.php");
+            die;
+        }
     } else {
         $product = array(
             'product_id' => $_POST['product_id'],
-            'quantity'=> 1
-   
+
         );
         $_SESSION['cart'][0] = $product;
-        array_push($product_ids,$_POST['product_id']);
+        array_push($product_ids, $_POST['product_id']);
     }
+    header("Location: index.php");
+    die;
 }
-
-print_r($product_ids);
-
-print_r($_SESSION['cart']);
 ?>
 
 <!DOCTYPE html>
@@ -64,51 +65,38 @@ print_r($_SESSION['cart']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="style.css" rel="stylesheet">
-    <title><?=$lang['title'] ?></title>
+    <title><?= $lang['title'] ?></title>
 </head>
 
 <body>
-    <h1><?=$lang['title'] ?></h1>
-    <h1>Welcome <?= $user_data['user_name']?></h1>
-    <a href="logout.php">Logout</a>
+    <h1><?= $lang['title'] ?></h1>
     <div class="container">
         <ul class="proditems">
-            <?php foreach ($result as $r): ?>
-                <?php if (!in_array($r['product_id'], $product_ids)): ?>
-                    <li>
-                        <form action="index.php" method="POST">
-                            <div class="proditem">
-                                <div class="prodimage">
-                                    <img src="uploads/<?= $r['product_image'] ?>">
-                                </div>
-                                <div class="proddetails">
-                                    <ul>
-                                        <li><?=$lang['product_title'] ?>: <?= $r['title'] ?></li>
-                                        <li><?=$lang['product_description'] ?>: <?= $r['description'] ?></li>
-                                        <li><?=$lang['product_price'] ?>: <?= $r['price'] ?>$ </li>
-                                    </ul>
-                                </div>
-                                <div class="addbutton">
-                                    <button type="submit" name="add">ADD</button>
-                                    <input type="hidden" name="product_id" value="<?= $r['product_id'] ?>">
-                                    
-                                </div>
-
+            <?php foreach ($result as $r) : ?>
+                <li>
+                    <form action="index.php" method="POST">
+                        <div class="proditem">
+                            <div class="prodimage">
+                                <img src="uploads/<?= $r['product_image'] ?>">
                             </div>
-                        </form>
-                    </li>
-                <?php endif; ?>
-
-
-
+                            <div class="proddetails">
+                                <ul>
+                                    <li><?= $lang['product_title'] ?>: <?= $r['title'] ?></li>
+                                    <li><?= $lang['product_description'] ?>: <?= $r['description'] ?></li>
+                                    <li><?= $lang['product_price'] ?>: <?= $r['price'] ?>$ </li>
+                                </ul>
+                            </div>
+                            <div class="addbutton">
+                                <button type="submit" name="add"><?= $lang['add'] ?></button>
+                                <input type="hidden" name="product_id" value="<?= $r['product_id'] ?>">
+                            </div>
+                        </div>
+                    </form>
+                </li>
             <?php endforeach; ?>
         </ul>
-        <a href="cart.php"><?=$lang['cart'] ?></a>
+        <a href="cart.php"><?= $lang['cart'] ?></a>
     </div>
-    <script>
-
-
-    </script>
-</body>
-
+            </body>
+            
 </html>
